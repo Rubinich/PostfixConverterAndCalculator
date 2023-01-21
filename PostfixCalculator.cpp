b61eed8c5b5039a16230c57d47890e0167773f62
Rubinich <alojzrubinic@gmail.com>
2023-01-21 21:19:17 +0100

    Update PostfixCalculator.cpp

--- PostfixCalculator.cpp
+++ PostfixCalculator.cpp
@@ -17,12 +17,15 @@ string InfixToPostfix(string infix) {
         char c = infix[i];
         if (isdigit(c)) {
             postfix += c;
+            // if the next number is operation make a whitespace
             if (isdigit(c) != isdigit(infix[i+1]))
                 postfix += ' ';
         }
         else if (c == '(')
             operations.push(c);
         else if (c == ')') {
+            /* if we reach ')' we have to save and pop all operations
+            sorted by priority*/
             while (!operations.empty() && operations.top() != '(') {
                 postfix += operations.top();
                 postfix += ' ';
@@ -31,14 +34,19 @@ string InfixToPostfix(string infix) {
             operations.pop();
         }
         else {
+            /*if the character in stack has higher priroity than the character in
+            infix we have to save them and pop all the characters with highter of
+            equal priroty*/
             while (!operations.empty() && priority(c) <= priority(operations.top())) {
                 postfix += operations.top();
                 postfix += ' ';
                 operations.pop();
             }
+            // pushing new operation from string
             operations.push(c);
         }
     }
+    // to get rid of leftovers(operations)
     while (!operations.empty()) {
         postfix += operations.top();
         postfix += ' ';
@@ -47,34 +55,21 @@ string InfixToPostfix(string infix) {
     return postfix;
 }
 
-/*int MultiDigit(stack<int> numbers) {
-    int number;
-    while (!numbers.empty()) {
-
-    }
-    return number;
-}*/
-
 double PostfixEvaluation(string postfix) {
     stack<int> numbers;
-    int t = 0;
     for (int i = 0; i < postfix.length();) {
         char  c = postfix[i];
         if (isdigit(c)) {
             numbers.push(c - '0');
-            t++;
             i++;
         }
         else if (c == ' ')
             i++;
-        else if (c == ' ' && isdigit(postfix[i-1])) {
-            //MultiDigit(numbers);
-            i++;
-            t = 0;
-        }
         else {
+            // second number
             int b = numbers.top();
             numbers.pop();
+            // first number
             int a = numbers.top();
             numbers.pop();
             switch (c) {
@@ -93,7 +88,8 @@ int main() {
     string infix;
     cout << "Enter infix: ";
     cin >> infix;
-    cout << "Postfix: " << InfixToPostfix(infix) << endl;
-    cout << "Solution: " << PostfixEvaluation(InfixToPostfix(infix)) << endl;
+    string postfix = InfixToPostfix(infix);
+    cout << "Postfix: " << postfix << endl;
+    cout << "Solution: " << PostfixEvaluation(postfix) << endl;
     return 0;
 }
